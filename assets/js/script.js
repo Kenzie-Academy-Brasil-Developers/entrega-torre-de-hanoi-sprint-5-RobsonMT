8b3c1daafb8fcafaf47558a6d4cc027e06d8adb6
RobsonMT <Rbsndev3@gmail.com>
2021-10-09 20:40:05 -0300

    refatorando-validações

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+let numMoviments = document.querySelector("#numMov").innerHTML = 0;
+let btnRestart = document.querySelector("#btnRestart").disabled = true;
+let btnStart = document.querySelector("#btnStart").disabled = false;
+
+document.querySelector("#btnStart").addEventListener('click', start)
+function start(){
+  document.querySelector("#btnRestart").disabled = false;
+  document.querySelector("#btnStart").disabled = true;
+  value();
+}
+
+function value(){
+  let output = '';
+  let num =  document.querySelector("#numDiscs").value;
+  document.getElementById("btnRestart").disabled = false;
+  numSelected.push(num);
+  
+  if(num == 3){output = 3, document.querySelector("#minMov").innerText = 7;};
+  if(num == 4){output = 4, document.querySelector("#minMov").innerText = 15;};
+  if(num == 5){output = 5, document.querySelector("#minMov").innerText = 31;};
+
+  return output;
+}
+
+const numSelected = [];
+let numStart = 5;
+
 function addDiscs(numb){
   for(i=numb;i>=1;i--){
     let arrayCores = ["#003F63", "#F2B138", "#A1A5A6", "#D9D9D9", "#350D40", "#02Ba38"];
@@ -15,9 +42,36 @@ function addDiscs(numb){
     disc.draggable = false;
     disc.classList.add('unselectable'),
     tower1.appendChild(disc);
-  }                       
+  }     
+  return addDiscs;
 }
-addDiscs(6)
+addDiscs(5)
+
+const blockDiscs = document.querySelectorAll(".disc") 
+blockDiscs.forEach(disck =>{
+  disck.addEventListener("mouseover", (event)=>{
+    // console.log(event.currentTarget)//disc
+    let tw1 = document.querySelector('#twr_1')//coluna-1
+    let tw2 = document.querySelector('#twr_2')//coluna-2
+    let tw3 = document.querySelector('#twr_3')//coluna-3
+
+    let dsc = event.currentTarget//disc
+
+    //verifico se são os ultimos filhos
+    if(dsc === tw1.lastChild || dsc === tw2.lastChild || dsc === tw3.lastChild){
+      event.currentTarget.classList.remove('unselectable'),
+      event.currentTarget.style.cursor = 'pointer',
+      event.currentTarget.draggable = true; 
+
+    }else {
+      //Caso não sejam bloqueio a movimentacão dos mesmos
+      event.currentTarget.classList.add('unselectable'),
+      event.currentTarget.style.cursor = 'no-drop',
+      event.currentTarget.draggable = false; 
+    
+    }
+  });
+})
 
 const discs = document.querySelectorAll(".disc") 
 discs.forEach(disc => {
@@ -27,8 +81,6 @@ discs.forEach(disc => {
 
 function dragStart(e){
   e.currentTarget.classList.add('moving');
-  // console.log(e.currentTarget)
-  // console.log(e.target)
 }
 
 function dragEnd(e){
@@ -38,19 +90,6 @@ function dragEnd(e){
 }
 
 const towers = document.querySelectorAll(".tower");
-
-towers.forEach(tower => {
-  tower.addEventListener('mouseover', (e)=>{
-    // console.log(e.target)//span
-    // console.log(e.currentTarget.querySelector('.disc'))//torre
-    if(e.currentTarget.querySelector('.disc') !== null ){
-      e.currentTarget.lastChild.classList.remove('unselectable'),
-      e.currentTarget.lastChild.style.cursor = 'pointer',
-      e.currentTarget.lastChild.draggable = true; 
-    }
-  })
-});
-
 //movimentos de drag sobre a torre
 towers.forEach(torre =>{
   torre.addEventListener("dragover", dragover);
@@ -61,7 +100,6 @@ towers.forEach(torre =>{
 //sempre que o item que esta com o evento passar na area
 function dragover(e){
   let itemID = document.querySelector('.moving');
-  console.log('aqui'+e.currentTarget.querySelector('.disc'))
 
   if(e.currentTarget.querySelector('.disc') === null || e.currentTarget.lastChild.id >= itemID.id){ 
      e.currentTarget.classList.add('yes');
@@ -82,11 +120,19 @@ function dragleave(e){
 function drop(e){
   let itemID = document.querySelector('.moving');
   //validação para permitir o drop nas colunas
-  if(e.currentTarget.querySelector('.disc') === null 
-  || e.currentTarget.lastChild.id >= itemID.id)
-  {  e.currentTarget.classList.add('yes');
-    e.currentTarget.appendChild(itemID)};
+  if(e.currentTarget.querySelector('.disc') === null || e.currentTarget.lastChild.id >= itemID.id){  
+    e.currentTarget.classList.add('yes');
+    e.currentTarget.appendChild(itemID);
+    //incremento n° de movimentos
+    document.querySelector("#numMov").innerHTML++;
+  };
   //removendo classes de indicação
   e.currentTarget.classList.remove('yes');
   e.currentTarget.classList.remove('noo');
+}
+
+document.querySelector("#btnRestart").addEventListener('click', restart)
+function restart(){
+  document.querySelector("#btnRestart").disabled = true;
+  document.querySelector("#btnStart").disabled = false;
 }
